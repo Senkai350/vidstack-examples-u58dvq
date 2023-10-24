d2d7fedd740fb963aee90d6fef8e1b9bc1a96afa
Administrator <test@example.test>
2023-10-24 20:11:43 +0300

    modified

---
+++ src/components/player/elements/TimeGroup.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="vds-time-group">
+    <media-time class="vds-time" type="current"></media-time>
+    <div class="vds-time-divider">/</div>
+    <media-time class="vds-time" type="duration"></media-time>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
